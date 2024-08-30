ce14c859737bce01cc414efb11867c1409311743
Owen Bick <12536625+bick@users.noreply.github.com>
2024-08-30 01:49:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Simple installer script for clutch
+# Simple installer script for noble
 # Supports Linux x86_64 and arm64 architectures
 
 set -e
@@ -13,10 +13,10 @@ main() {
     if [ "$os" = "Linux" ]; then
         case $arch in
             x86_64)
-                clutch_uri="https://owenbick.com/releases/Linux_x86_64.tar.gz"
+                noble_uri="https://github.com/bick/noble-cli/archive/refs/tags/test.zip"
                 ;;
             arm64)
-                clutch_uri="https://owenbick.com/releases/Linux_arm64.tar.gz"
+                noble_uri="https://github.com/bick/noble-cli/archive/refs/tags/test.zip"
                 ;;
             *)
                 echo "Error: Unsupported architecture $arch" 1>&2
@@ -28,37 +28,37 @@ main() {
         exit 1
     fi
 
-    clutch_install="${CLUTCH_INSTALL:-$HOME/.clutch}"
+    noble_install="${NOBLE_INSTALL:-$HOME/.noble}"
 
-    bin_dir="$clutch_install/bin"
-    tmp_dir="$clutch_install/tmp"
-    exe="$bin_dir/clutch"
+    bin_dir="$noble_install/bin"
+    tmp_dir="$noble_install/tmp"
+    exe="$bin_dir/noble"
 
     mkdir -p "$bin_dir"
     mkdir -p "$tmp_dir"
 
-    echo "Downloading clutch from $clutch_uri..."
-    curl -q --fail --location --progress-bar --output "$tmp_dir/clutch.tar.gz" "$clutch_uri"
+    echo "Downloading noble from $noble_uri..."
+    curl -q --fail --location --progress-bar --output "$tmp_dir/noble.tar.gz" "$noble_uri"
 
     # Extract to tmp dir so we don't open the existing executable file for writing:
-    tar -C "$tmp_dir" -xzf "$tmp_dir/clutch.tar.gz"
-    chmod +x "$tmp_dir/clutch"
+    tar -C "$tmp_dir" -xzf "$tmp_dir/noble.tar.gz"
+    chmod +x "$tmp_dir/noble"
 
     # Atomically rename into place:
-    mv "$tmp_dir/clutch" "$exe"
-    rm "$tmp_dir/clutch.tar.gz"
+    mv "$tmp_dir/noble" "$exe"
+    rm "$tmp_dir/noble.tar.gz"
 
-    echo "clutch was installed successfully to $exe"
-    if command -v clutch >/dev/null; then
-        echo "Run 'clutch --help' to get started"
+    echo "noble was installed successfully to $exe"
+    if command -v noble >/dev/null; then
+        echo "Run 'noble --help' to get started"
     else
         case $SHELL in
         /bin/zsh) shell_profile=".zshrc" ;;
         *) shell_profile=".bash_profile" ;;
         esac
         echo "Manually add the directory to your \$HOME/$shell_profile (or similar):"
-        echo "  export CLUTCH_INSTALL=\"$clutch_install\""
-        echo "  export PATH=\"\$CLUTCH_INSTALL/bin:\$PATH\""
+        echo "  export NOBLE_INSTALL=\"$noble_install\""
+        echo "  export PATH=\"\$NOBLE_INSTALL/bin:\$PATH\""
         echo "Run '$exe --help' to get started"
     fi
 }
