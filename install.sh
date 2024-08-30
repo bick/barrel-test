732301ab3b5748c9034ebf77d93006c164c26951
Owen Bick <12536625+bick@users.noreply.github.com>
2024-08-30 02:07:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,23 +10,20 @@ main() {
     version=${1:-latest}
 
     # Determine the correct binary based on architecture
-    if [ "$os" = "Linux" ]; then
-        case $arch in
-            x86_64)
-                noble_uri="https://github.com/bick/noble-cli/releases/download/test/noble-x86_64.tar.gz"
-                ;;
-            arm64)
-                noble_uri="https://github.com/bick/noble-cli/releases/download/test/noble-arm64.tar.gz"
-                ;;
-            *)
-                echo "Error: Unsupported architecture $arch" 1>&2
-                exit 1
-                ;;
-        esac
-    else
-        echo "Error: Unsupported OS $os" 1>&2
-        exit 1
-    fi
+    case $arch in
+        x86_64)
+            noble_uri="https://github.com/bick/noble-cli/releases/download/test/noble-x86_64.tar.gz"
+            binary_name="noble-x86_64"
+            ;;
+        arm64)
+            noble_uri="https://github.com/bick/noble-cli/releases/download/test/noble-arm64.tar.gz"
+            binary_name="noble-arm64"
+            ;;
+        *)
+            echo "Error: Unsupported architecture $arch" 1>&2
+            exit 1
+            ;;
+    esac
 
     noble_install="${NOBLE_INSTALL:-$HOME/.noble}"
 
@@ -40,13 +37,19 @@ main() {
     echo "Downloading noble from $noble_uri..."
     curl -q --fail --location --progress-bar --output "$tmp_dir/noble.tar.gz" "$noble_uri"
 
-    # Extract to tmp dir so we don't open the existing executable file for writing:
+    # Extract to tmp dir so we don't open the existing executable file for writing
     tar -C "$tmp_dir" -xzf "$tmp_dir/noble.tar.gz"
-    chmod +x "$tmp_dir/noble"
 
-    # Atomically rename into place:
-    mv "$tmp_dir/noble" "$exe"
-    rm "$tmp_dir/noble.tar.gz"
+    # Check if the expected binary is there
+    if [ ! -f "$tmp_dir/$binary_name" ]; then
+        echo "Error: $binary_name not found in the extracted tarball." 1>&2
+        exit 1
+    fi
+
+    # Set executable permissions and move the binary to the bin directory
+    chmod +x "$tmp_dir/$binary_name"
+    mv "$tmp_dir/$binary_name" "$exe"
+    rm -rf "$tmp_dir"
 
     echo "noble was installed successfully to $exe"
 
